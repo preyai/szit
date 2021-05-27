451427a098b82ac93b4290645fcfffed83f8d68c
preyaizman <preyizman@gmail.com>
2021-05-27 17:47:33 +0300

    day3

--- js/szit.js
+++ js/szit.js
@@ -2,18 +2,18 @@
 $(function () {
     // init
     new Splide('.splide').mount();
-
+    new Splide('.splide2', {
+        type    : 'loop',
+        perPage : 3,
+        autoplay: true,
+    }).mount();
+    $('.select2').select2();
 
     console.log('start');
     $('.hamburger').on('click', function () {
         $(this).toggleClass('active');
     });
-    $('.square').each(function(element) {
-        // setHeight(element);
+    $('.square').each(function (element) {
         $(this).height($(this).width());
     });
-    function setHeight(item) {
-        // console.log(this);
-        this.height(this.width())
-    }
 })
