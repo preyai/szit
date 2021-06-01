d3c13c9a892ce5fa80304e3901bd1a15f7b1b479
preyaizman <preyizman@gmail.com>
2021-06-01 12:00:34 +0300

    02

--- js/szit.js
+++ js/szit.js
@@ -1,18 +1,8 @@
 
 $(function () {
     // init
-    new Splide('.banners').mount();
-    new Splide('#galery1', {
-        type: 'loop',
-        perPage: 6,
-        perMove: 1,
-    }).mount();
-    new Splide('#galery2', {
-        type: 'loop',
-        perPage: 6,
-        perMove: 1,
-    }).mount();
-    new Splide('.post_slider').mount();
+    
+    // 
     $('.select2').select2({
         width: 'max-content'
     });
